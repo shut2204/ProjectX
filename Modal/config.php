0058fc60d9f528378076f2c013dfe78171fb7caf
shut2204 <80090271+shut2204@users.noreply.github.com>
2022-11-28 11:23:23 +0200

    Update config.php

--- Modal/config.php
+++ Modal/config.php
@@ -1,7 +1,7 @@
 <?php
 
 return [
-    'host' => 'http://eu-cdbr-west-03.cleardb.net',
+    'host' => 'eu-cdbr-west-03.cleardb.net',
     'db_name' => 'heroku_a5eaf22cd9d6433',
     'username' => 'b9d0ad253389b0',
     'password' => 'cff26802',
